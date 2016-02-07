0d37558de95b9d5d2d7d947d0b0bdd7bad6c29e8
Roselle Ebarle <roselle.ebarle04@gmail.com>
2016-02-07 22:46:22 +0800

    Update working tests on creating collection

--- tests.py
+++ tests.py
@@ -1,28 +1,31 @@
 import unittest
 import json
-from app import app
+import os
+import app
+import tempfile 
 from app import db
 from app.models import *
+
+TEST_DB = 'test.db'
 
 class AppTestCase(unittest.TestCase):
 
     def setUp(self):
-        # self.app = create_app('testing')
-        self.app = app
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        app.app.config['TESTING'] = True
+        app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, TEST_DB)
+        self.app = app.app.test_client()
         db.create_all()
-        self.client = self.app.test_client(use_cookies=True)
 
     def tearDown(self):
-        db.session.remove()
         db.drop_all()
-        self.app_context.pop()
 
     def test_create_new_collection(self):
         json_data = json.dumps(dict(member_id=1, or_number=12345))
-        response = self.client.post('/api/collections/', content_type='application/json', data=json_data)
+        response = self.app.post('/api/collections/', content_type='application/json', data=json_data)
         self.assertEqual(response.status_code, 201)
+
+
 
 if __name__ == '__main__':
     unittest.main()
